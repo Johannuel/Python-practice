5473d4b5f1418252e833fe7d77c791ae871a671c
MFV coder <johanunuelmota@gmail.com>
2026-05-24 15:58:06 -0400

    Update Rank.py

--- Rank.py
+++ Rank.py
@@ -1,12 +1,23 @@
+from posix import stat
+
 personajes = {
-    "Eliot Alderson": {"poder": 50, "destreza": 90, "velocidad": 60},
-    "Superman": {"poder": 100, "destreza": 80, "velocidad": 95},
+    "Superman": {"poder": 100, "destreza": 80, "velocidad": 95, "inteligencia": 70},
+    "Batman": {"poder": 70, "destreza": 60, "velocidad": 80, "inteligencia": 90},
+    "Homelander": {"poder": 100, "destreza": 80, "velocidad": 95, "inteligencia": 40},
+    "Butcher": {"poder": 90, "destreza": 70, "velocidad": 85, "inteligencia": 80},
+    "Flash": {"poder": 80, "destreza": 70, "velocidad": 80, "inteligencia": 180},
+    "Wally west": {"poder": 85, "destreza": 75, "velocidad": 85, "inteligencia": 200},
+    "Wonder Woman": {"poder": 90, "destreza": 80, "velocidad": 90, "inteligencia": 100},
+    "Hulk": {"poder": 100, "destreza": 90, "velocidad": 100, "inteligencia": 80},
 }
 
 
 def calcular_ranking(stats):
     total = (
-        (stats["poder"] * 0.4) + (stats["destreza"] * 0.3) + (stats["velocidad"] * 0.3)
+        (stats["poder"] * 0.4)
+        + (stats["destreza"] * 0.3)
+        + (stats["velocidad"] * 0.3)
+        + (stats["inteligencia"] * 0.3)
     )
     return total
 
